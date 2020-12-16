5a79ccb929085ac1d5a8834179561649e8226fc1
vaux <zero.griffin@gmail.com>
2020-12-16 16:28:59 +0100

    Sort pages

--- Sources/Files.swift
+++ Sources/Files.swift
@@ -2,19 +2,18 @@ import Foundation
 import Combine
 
 public extension FileManager {
-    static var pages: Future<Set<Page>, Never> {
+    static var pages: Future<[Page], Never> {
         .init { result in
             queue.async {
                 guard instance.fileExists(atPath: folder.path) else { return result(.success([])) }
                 result(
                     .success(
-                        .init((try? instance.contentsOfDirectory(at: folder, includingPropertiesForKeys: [], options: .skipsHiddenFiles))
-                                .map {
-                                    $0.compactMap {
-                                        try? JSONDecoder().decode(Page.self, from: .init(contentsOf: $0))
-                                    }
-                                } ?? []
-                        )
+                        (try? instance.contentsOfDirectory(at: folder, includingPropertiesForKeys: [], options: .skipsHiddenFiles))
+                            .map {
+                                $0.compactMap {
+                                    try? JSONDecoder().decode(Page.self, from: .init(contentsOf: $0))
+                                }
+                            }?.sorted { $0.date < $1.date } ?? []
                     )
                 )
             }
