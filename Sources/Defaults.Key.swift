7d5ea705adcb1b87d8b108e5108cbca00d8f25a7
vaux <zero.griffin@gmail.com>
2020-12-16 12:15:30 +0100

    Add defaults

---
+++ Sources/Defaults.Key.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+public extension Defaults {
+    enum Key: String {
+        case
+        premium,
+        rated,
+        popups,
+        javascript,
+        ads,
+        secure,
+        trackers,
+        cookies,
+        dark,
+        engine,
+        created
+    }
+}
---
+++ Sources/Defaults.swift
@@ -0,0 +1,67 @@
+import Foundation
+
+public final class Defaults: UserDefaults {
+    public class var premium: Bool {
+        get { self[.premium] as? Bool ?? false }
+        set { self[.premium] = newValue }
+    }
+    
+    public class var rated: Bool {
+        get { self[.rated] as? Bool ?? false }
+        set { self[.rated] = newValue }
+    }
+    
+    public class var popups: Bool {
+        get { self[.popups] as? Bool ?? true }
+        set { self[.popups] = newValue }
+    }
+    
+    public class var javascript: Bool {
+        get { self[.javascript] as? Bool ?? true }
+        set { self[.javascript] = newValue }
+    }
+    
+    public class var ads: Bool {
+        get { self[.ads] as? Bool ?? true }
+        set { self[.ads] = newValue }
+    }
+    
+    public class var trackers: Bool {
+        get { self[.trackers] as? Bool ?? true }
+        set { self[.trackers] = newValue }
+    }
+    
+    public class var cookies: Bool {
+        get { self[.cookies] as? Bool ?? true }
+        set { self[.cookies] = newValue }
+    }
+    
+    public class var secure: Bool {
+        get { self[.secure] as? Bool ?? true }
+        set { self[.secure] = newValue }
+    }
+    
+    public class var dark: Bool {
+        get { self[.dark] as? Bool ?? true }
+        set { self[.dark] = newValue }
+    }
+    
+    public class var engine: Engine {
+        get { (self[.engine] as? String).flatMap(Engine.init(rawValue:)) ?? .ecosia }
+        set { self[.engine] = newValue.rawValue }
+    }
+    
+    public class var created: Date? {
+        get { self[.created] as? Date }
+        set { self[.created] = newValue }
+    }
+    
+    private class subscript(_ key: Key) -> Any? {
+        get {
+            standard.object(forKey: key.rawValue)
+        }
+        set {
+            standard.setValue(newValue, forKey: key.rawValue)
+        }
+    }
+}
