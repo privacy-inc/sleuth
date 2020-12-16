547fdc4013033d694dda3ed06ec9ba3a474cfdc6
vaux <zero.griffin@gmail.com>
2020-12-16 13:49:06 +0100

    Add tabs

---
+++ Sources/Tabs.swift
@@ -0,0 +1,7 @@
+import Foundation
+import Combine
+
+public final class Tabs {
+    public let tabs = CurrentValueSubject<[Page], Never>([])
+    public let selected = CurrentValueSubject<Page?, Never>(nil)
+}
