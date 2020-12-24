2b92abe32cb7825e28d4c1e95f9d639e56e40c3b
vaux <zero.griffin@gmail.com>
2020-12-24 13:41:46 +0200

    Update scripts

--- Sources/Scripts.swift
+++ Sources/Scripts.swift
@@ -1,7 +1,7 @@
 import Foundation
 
-public struct Dark {
-    public static let script = """
+public struct Scripts {
+    public static let dark = """
 function dark() {
     if (document.styleSheets && document.styleSheets.length > 0) {
         for (var i = 0; i < document.styleSheets.length; i++) {
@@ -9,7 +9,7 @@ function dark() {
                 for (var j = 0; j < document.styleSheets[i].cssRules.length; j++) {
                     if (document.styleSheets[i].cssRules[j] instanceof CSSMediaRule &&
                         document.styleSheets[i].cssRules[j].media.mediaText.indexOf('dark') > 0) {
-                            return true;
+                         /Users/vaux/dev/Sleuth/Sources/Dark.swift   return true;
                     }
                 }
             }
@@ -24,5 +24,18 @@ if (!dark()) {
     style.innerHTML = ":root, img, [style*=background-image], [class*=video-thumbnail-img], #player-container-id { filter: invert(1) hue-rotate(.5turn); }";
     document.head.appendChild(style);
 }
+if true {
+    var style = document.createElement('style');
+    style.innerHTML = ":root, img, [style*=background-image], [class*=video-thumbnail-img], #player-container-id { filter: invert(1) hue-rotate(.5turn); }";
+    document.head.appendChild(style);
+}
+"""
+    
+    public static let scroll = """
+    if (location.host.includes("google.com")) {
+        var style = document.createElement('style');
+        style.innerHTML = ":root { overflow-y: visible !important; }";
+        document.head.appendChild(style);
+    }
 """
 }
