5f0eb9d2e59ebb1aa48eeff24a7c034e184ff334
vaux <zero.griffin@gmail.com>
2020-12-25 00:49:10 +0200

    Update browser

--- Sources/Browser.swift
+++ Sources/Browser.swift
@@ -6,11 +6,13 @@ public final class Browser {
     public let error = CurrentValueSubject<String?, Never>(nil)
     public let backwards = CurrentValueSubject<Bool, Never>(false)
     public let forwards = CurrentValueSubject<Bool, Never>(false)
+    public let loading = CurrentValueSubject<Bool, Never>(false)
     public let progress = CurrentValueSubject<Double, Never>(0)
     public let browse = PassthroughSubject<URL, Never>()
     public let previous = PassthroughSubject<Void, Never>()
     public let next = PassthroughSubject<Void, Never>()
     public let reload = PassthroughSubject<Void, Never>()
+    public let stop = PassthroughSubject<Void, Never>()
     private var subscription: AnyCancellable?
     
     public init() {
