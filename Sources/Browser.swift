45c0599530bd64a920cf94d733003ce52f7ce198
vaux <zero.griffin@gmail.com>
2020-12-31 13:31:29 +0100

    Update protocol

--- Sources/Browser.swift
+++ Sources/Browser.swift
@@ -2,19 +2,19 @@ import Foundation
 import Combine
 
 public protocol Browser {
-    var progress: Double { get set }
-    var loading: Bool { get set }
-    var title: String { get set }
-    var url: URL { get set }
-    var backwards: Bool { get set }
-    var forwards: Bool { get set }
-    var error: String? { get set }
     var browse: PassthroughSubject<URL, Never> { get }
     var previous: PassthroughSubject<Void, Never> { get }
     var next: PassthroughSubject<Void, Never> { get }
     var reload: PassthroughSubject<Void, Never> { get }
     var stop: PassthroughSubject<Void, Never> { get }
     
+    func progress(_ value: Double)
+    func loading(_ value: Bool)
+    func title(_ value: String)
+    func url(_ value: URL)
+    func backwards(_ value: Bool)
+    func forwards(_ value: Bool)
+    func error(_ value: String?)
     func popup(_ url: URL)
     func external(_ url: URL)
     func blocked(_ domain: String)
