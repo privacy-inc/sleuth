b9e5c3760564160402fe317b81d820ab4ee04871
vaux <zero.griffin@gmail.com>
2021-01-02 14:47:27 +0100

    Make page equatable

--- Sources/Share.Page.swift
+++ Sources/Share.Page.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 public extension Share {
-    struct Page: Codable {
+    struct Page: Codable, Equatable {
         public let url: URL
         public let title: String
         public let subtitle: String
