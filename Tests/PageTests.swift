14af3f7c69952209103bf1cfde12721b36d0f0c0
vaux <zero.griffin@gmail.com>
2021-01-02 15:38:10 +0100

    Update test

--- Tests/PageTests.swift
+++ Tests/PageTests.swift
@@ -5,7 +5,7 @@ final class PageTests: XCTestCase {
     func testItem() {
         var page = Page(url: URL(string: "https://aguacate.com")!)
         page.title = "Aguacate"
-        XCTAssertEqual(URL(string: "privacy_id://" + page.id.uuidString), page.shared.url)
+        XCTAssertEqual(URL(string: "privacy-id://" + page.id.uuidString), page.shared.url)
         XCTAssertEqual("Aguacate", page.shared.title)
         XCTAssertEqual("https://aguacate.com", page.shared.subtitle)
     }
