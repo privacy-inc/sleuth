57436a7e0d4824db6deead06cf9a4f4f40231851
vaux <zero.griffin@gmail.com>
2021-01-08 17:05:14 +0100

    Add location script

--- Sources/Scripts.swift
+++ Sources/Scripts.swift
@@ -35,4 +35,30 @@ if (!dark()) {
     }
 
 """
+    
+    public static let location = """
+var locationSuccess = null;
+
+function locationReceived(latitude, longitude, accuracy) {
+    var position = {
+        coords: {
+            latitude: latitude,
+            longitude: longitude,
+            accuracy: accuracy
+        }
+    };
+
+    if (locationSuccess != null) {
+        locationSuccess(position);
+    }
+
+    locationSuccess = null;
+}
+
+navigator.geolocation.getCurrentPosition = function(success, error, options) {
+    locationSuccess = success;
+    window.webkit.messageHandlers.handler.postMessage('getCurrentPosition');
+};
+
+"""
 }
