b06f6a9b06f87d27899a81b253a7f31edf1e9242
vaux <zero.griffin@gmail.com>
2021-01-13 11:04:03 +0100

    Update dark script

--- Sources/Scripts.swift
+++ Sources/Scripts.swift
@@ -3,6 +3,9 @@ import Foundation
 public struct Scripts {
     public static let dark = """
 function supports_dark() {
+    if (document.documentElement.getAttribute("dark") == "true") { // youtube
+        return true;
+    }
     if (document.styleSheets && document.styleSheets.length > 0) {
         for (var i = 0; i < document.styleSheets.length; i++) {
             if (document.styleSheets[i].cssRules) {
@@ -20,27 +23,26 @@ function supports_dark() {
 }
 
 if (!supports_dark()) {
-    var google_related_searches = ".JAC8bd *, ";
-    var google_related_top = ".keP9hb, .XAOBve *, ";
-    var google_result_images = "#media_result_group *, ";
-    var google_result_video_big = ".twQ0Be *, ";
-    var google_result_video_small = ".JWCxk *, .pP3ABc *, ";
-    var google_images_square = ".c7cjWc, ";
-    var google_images_circle = ".Qjibbc, ";
-    var google_videos = ".N3nEGc *, ";
-    var youtube_desktop = "#player *, ";
-    var youtube_mobile = "#player-container-id *";
-    
-    var exclude = google_related_searches + google_related_top + google_result_images + google_result_video_big + google_result_video_small + google_images_square + google_images_circle + google_videos + youtube_desktop + youtube_mobile;
+    var exclude = [
+                                            // Google
+        ".JAC8bd *",                        //// related searches
+        ".keP9hb, .XAOBve *",               //// related top
+        "#media_result_group *",            //// result images
+        ".twQ0Be *",                        //// result video big
+        ".JWCxk *, .pP3ABc *",              //// result video small
+        ".c7cjWc",                          //// images square
+        ".Qjibbc",                          //// images circle
+        ".N3nEGc *"                         //// videos
+    ];
     var style = document.createElement('style');
-    style.innerHTML = "*:not(" + exclude + ") { background-color: #252228 !important; color: #cecccf !important; border-color: #454248 !important ; }";
+    style.innerHTML = "*:not(" + exclude.join(", ") + ") { background-color: #252228 !important; color: #cecccf !important; border-color: #454248 !important ; }";
     document.head.appendChild(style);
 }
 
 """
     
     public static let scroll = """
-    if (location.host.includes("google.com") || location.host.includes("youtube.com")) {
+    if (location.host.includes("google.com")) {
         var style = document.createElement('style');
         style.innerHTML = ":root { overflow-y: visible !important; }";
         document.head.appendChild(style);
