4e1fde07d171f280478d251f36051a32b59a6f8d
vaux <zero.griffin@gmail.com>
2021-04-06 15:13:32 +0200

    Pinterest

--- Sources/Block.swift
+++ Sources/Block.swift
@@ -65,7 +65,7 @@ public struct Block {
         .init(.css(".tp-modal"), .init(site: .thelocal)),
         .init(.css(".tp-backdrop"), .init(site: .thelocal)),
         .init(.css(".Jea.LCN.Lej.PKX._he.dxm.fev.fte.gjz.jzS.ojN.p6V.qJc.zI7.iyn.Hsu"), .init(site: .pinterest)),
-//        .init(.css(".zI7.iyn.Hsu"), .init(site: .pinterest))
+        .init(.css(".QLY.Rym.ZZS._he.ojN.p6V.zI7.iyn.Hsu"), .init(site: .pinterest))
     ])
     
     public static let dark = serialise(rules: [
