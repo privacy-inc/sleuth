da82a4e921e79768d9760c849a4e6d71d2bea419
vaux <zero.griffin@gmail.com>
2021-04-22 15:22:45 +0200

    Update scroll script

--- Sources/Scripts.Scroll.swift
+++ Sources/Scripts.Scroll.swift
@@ -2,29 +2,29 @@ import Foundation
 
 extension Scripts {
     public static let scroll = """
-    if (location.host.includes("google.com")) {
+    if (location.hostname.endsWith("google.com")) {
         var style = document.createElement('style');
         style.innerHTML = ":root, body { overflow-y: visible !important; }";
         document.head.appendChild(style);
-    } else if (location.host.includes("youtube.com")) {
+    } else if (location.hostname.endsWith("youtube.com")) {
         var style = document.createElement('style');
         style.innerHTML = "body { position: unset !important; }";
         document.head.appendChild(style);
-    } else if (location.host.includes("instagram.com")) {
+    } else if (location.hostname.endsWith("instagram.com")) {
         var style = document.createElement('style');
         style.innerHTML = "body, .E3X2T { overflow: unset !important; }";
         document.head.appendChild(style);
-    } else if (location.host.includes("reuters.com")) {
+    } else if (location.hostname.endsWith("reuters.com")) {
         var style = document.createElement('style');
         style.innerHTML = ":root, html, body { overflow: unset !important; }";
         document.head.appendChild(style);
-    } else if (location.host.includes("thelocal.de")) {
+    } else if (location.hostname.endsWith("thelocal.de")) {
         var style = document.createElement('style');
         style.innerHTML = ".tp-modal-open { overflow: unset !important; }";
         document.head.appendChild(style);
-    } else if (location.host.includes("pinterest.com")) {
+    } else if (location.hostname.endsWith("pinterest.com")) {
         document.body.setAttribute("style", "overflow-y: visible !important");
-    } else if (location.host.includes("bbc.com")) {
+    } else if (location.hostname.endsWith("bbc.com")) {
         var style = document.createElement('style');
         style.innerHTML = "body { overflow: unset !important; }";
         document.head.appendChild(style);
