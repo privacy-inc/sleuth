ab0576e4299e692caa084a580443714600e2a80b
vaux <zero.griffin@gmail.com>
2021-04-27 10:26:56 +0200

    Add bookmark

--- Sources/Entry.Bookmark.swift
+++ Sources/Entry.Bookmark.swift
@@ -2,7 +2,29 @@ import Foundation
 import Archivable
 
 extension Entry {
-    public struct Bookmark: Equatable {
+    enum Bookmark: Equatable, Property {
+        case
+        remote(String),
+        local(String, Data)
         
+        var url: String {
+            switch self {
+            case let .remote(url), let .local(url, _):
+                return url
+            }
+        }
+        
+        var data: Data {
+            Data()
+                .adding(UInt16(id))
+        }
+        
+        init(data: inout Data) {
+            self = .remote(.init(data.uInt16()), .init(url: "", title: ""))
+        }
+        
+        init(url: String) {
+            
+        }
     }
 }
--- Sources/Entry.swift
+++ Sources/Entry.swift
@@ -1,51 +1,35 @@
 import Foundation
 import Archivable
 
-public enum Entry: Equatable, Property {
-    case
-    remote(Int, Info),
-    local(Int, Info, Bookmark)
-    
-    public var id: Int {
-        switch self {
-        case let .local(id, _, _), let .remote(id, _):
-            return id
-        }
-    }
+public struct Entry: Equatable, Property {
+    public let title: String
+    public let date: Date
+    let id: Int
+    let bookmark: Bookmark
     
     public var url: String {
-        info.url
-    }
-    
-    public var title: String {
-        info.title
-    }
-    
-    public var date: Date {
-        info.date
-    }
-    
-    private var info: Info {
-        switch self {
-        case let .local(_, info, _), let .remote(_, info):
-            return info
-        }
+        bookmark.url
     }
     
     public var data: Data {
         Data()
-            .adding(UInt16(id))
+            .adding(date)
+            .adding(title)
+            .adding(bookmark.data)
     }
     
     public init(data: inout Data) {
-        self = .remote(.init(data.uInt16()), .init(url: "", title: ""))
+        id = 0
+        date = data.date()
+        title = data.string()
+        url = data.string()
     }
     
-    init(id: Int, url: String) {
-        self = .remote(id, .init(url: "", title: ""))
-    }
-    
-    func with(title: String) -> Self {
-        self
+    init(id: Int, title: String, url: String) {
+        self.id = id
+        
+        self.title = title
+        self.url = url
+        date = .init()
     }
 }
