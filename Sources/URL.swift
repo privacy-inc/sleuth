8ab2db62c5b14c9c89aacf099d4567f3b227751f
vaux <zero.griffin@gmail.com>
2021-04-30 12:40:28 +0200

    Update url

--- Sources/URL.swift
+++ Sources/URL.swift
@@ -1,9 +1,15 @@
 import Foundation
 
 extension URL {
+#if os(iOS) || os(macOS)
     var bookmark: Data {
         (try? bookmarkData(options: .withSecurityScope)) ?? .init()
     }
+#else
+    var bookmark: Data {
+        .init()
+    }
+#endif
     
     var schemeless: String {
         scheme == nil ? absoluteString : .init(absoluteString.dropFirst(scheme!.count + 3))
