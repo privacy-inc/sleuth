cf16a74a1f1a8a1b68be4854ea8f72b1033512a1
vaux <zero.griffin@gmail.com>
2021-05-06 18:34:52 +0200

    Update domain

--- Sources/Array.Blocker.swift
+++ Sources/Array.Blocker.swift
@@ -30,7 +30,7 @@ extension Array where Element == Blocker.Rule.Trigger {
                 return """
 "if-domain": ["\(domain
                     .map {
-                        "*" + $0
+                        "*" + $0 + "*"
                     }
                     .joined(separator: ", "))"]
 """
