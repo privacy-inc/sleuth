c2cda9add882aa77948fe189f55740355df3aa1f
vaux <zero.griffin@gmail.com>
2021-05-06 15:00:13 +0200

    Update urls

--- Sources/URL.Subdomain.swift
+++ Sources/URL.Subdomain.swift
@@ -4,31 +4,7 @@ extension URL {
     enum Subdomain: String {
         case
         sourcepoint,
-        crwdcntrl,
         tags,
-        casalemedia,
-        safeframe,
-        demdex,
-        adition,
-        openx,
-        guim,
-        imasdk,
-        googletagservices,
-        optimizely,
-        cedexis,
-        scorecardresearch,
-        bounceexchange,
-        zeusadx,
-        adxnexus,
-        revrtb,
-        acertb,
-        popmonetizer,
-        tiodmw,
-        ratappe,
-        riverhit,
-        ahojer,
-        impactserving,
-        f853150605ccb,
-        cedexis_radar = "cedexis-radar"
+        guim
     }
 }
