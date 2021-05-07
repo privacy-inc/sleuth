682347f0065bf8f148eda5e475dd3f4fd92c2378
vaux <zero.griffin@gmail.com>
2021-05-07 14:01:48 +0200

    Update tests

--- Tests/BlockerTests.swift
+++ Tests/BlockerTests.swift
@@ -44,71 +44,47 @@ final class BlockerTests: XCTestCase {
                                                             "#lb",
                                                             ".hww53CMqxtL__mobile-promo",
                                                             "#Sx9Kwc"]))
+        
         XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
                         .css(url: "youtube.com", selectors: ["#consent-bump",
                                                              ".opened",
                                                              ".ytd-popup-container",
                                                              ".upsell-dialog-lightbox",
                                                              ".consent-bump-lightbox"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "instagram.com", selectors: [".RnEpo.Yx5HN",
+                                                               ".RnEpo._Yhr4",
+                                                               ".R361B",
+                                                               ".NXc7H.jLuN9.X6gVd",
+                                                               ".f11OC"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "reuters.com", selectors: ["#onetrust-consent-sdk",
+                                                             "#newReutersModal"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "twitter.com", selectors: [
+                                ".css-1dbjc4n.r-aqfbo4.r-1p0dtai.r-1d2f490.r-12vffkv.r-1xcajam.r-zchlnj"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "thelocal.de", selectors: ["#qc-cmp2-container",
+                                                             ".tp-modal",
+                                                             ".tp-backdrop"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "pinterest.com", selectors: [
+                                ".Jea.LCN.Lej.PKX._he.dxm.fev.fte.gjz.jzS.ojN.p6V.qJc.zI7.iyn.Hsu",
+                                ".QLY.Rym.ZZS._he.ojN.p6V.zI7.iyn.Hsu"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "bbc.com", selectors: [".fc-consent-root",
+                                                         "#cookiePrompt",
+                                                         ".ssrcss-u3tmht-ConsentBanner.exhqgzu6"]))
+        
+        XCTAssertTrue(Parser(content: Blocker.rules([.popups]))
+                        .css(url: "reddit.com", selectors: ["._3q-XSJ2vokDQrvdG6mR__k",
+                                                            ".EUCookieNotice",
+                                                            ".XPromoPopup"]))
     }
 }
-
-
-/*
-
-
-func testGoogle() {
-    XCTAssertTrue(rules.displayNone(domain: "*google.com", selector: "#consent-bump"))
-    XCTAssertTrue(rules.displayNone(domain: "*google.com", selector: "#lb"))
-    XCTAssertTrue(rules.displayNone(domain: "*google.com", selector: ".hww53CMqxtL__mobile-promo"))
-    XCTAssertTrue(rules.displayNone(domain: "*google.com", selector: "#Sx9Kwc"))
-}
-
-func testYouTube() {
-    XCTAssertTrue(rules.displayNone(domain: "*youtube.com", selector: "#consent-bump"))
-    XCTAssertTrue(rules.displayNone(domain: "*youtube.com", selector: ".opened"))
-    XCTAssertTrue(rules.displayNone(domain: "*youtube.com", selector: ".ytd-popup-container"))
-    XCTAssertTrue(rules.displayNone(domain: "*youtube.com", selector: ".upsell-dialog-lightbox"))
-    XCTAssertTrue(rules.displayNone(domain: "*youtube.com", selector: ".consent-bump-lightbox"))
-}
-
-func testInstagram() {
-    XCTAssertTrue(rules.displayNone(domain: "*instagram.com", selector: ".RnEpo.Yx5HN"))
-    XCTAssertTrue(rules.displayNone(domain: "*instagram.com", selector: ".RnEpo._Yhr4"))
-    XCTAssertTrue(rules.displayNone(domain: "*instagram.com", selector: ".R361B"))
-    XCTAssertTrue(rules.displayNone(domain: "*instagram.com", selector: ".NXc7H.jLuN9.X6gVd"))
-    XCTAssertTrue(rules.displayNone(domain: "*instagram.com", selector: ".f11OC"))
-}
-
-func testReuters() {
-    XCTAssertTrue(rules.displayNone(domain: "*reuters.com", selector: "#onetrust-consent-sdk"))
-    XCTAssertTrue(rules.displayNone(domain: "*reuters.com", selector: "#newReutersModal"))
-}
-
-func testTwitter() {
-    XCTAssertTrue(rules.displayNone(domain: "*twitter.com", selector: ".css-1dbjc4n.r-aqfbo4.r-1p0dtai.r-1d2f490.r-12vffkv.r-1xcajam.r-zchlnj"))
-}
-
-func testTheLocal() {
-    XCTAssertTrue(rules.displayNone(domain: "*thelocal.de", selector: "#qc-cmp2-container"))
-    XCTAssertTrue(rules.displayNone(domain: "*thelocal.de", selector: ".tp-modal"))
-    XCTAssertTrue(rules.displayNone(domain: "*thelocal.de", selector: ".tp-backdrop"))
-}
-
-func testPinterest() {
-    XCTAssertTrue(rules.displayNone(domain: "*pinterest.com", selector: ".Jea.LCN.Lej.PKX._he.dxm.fev.fte.gjz.jzS.ojN.p6V.qJc.zI7.iyn.Hsu"))
-    XCTAssertTrue(rules.displayNone(domain: "*pinterest.com", selector: ".QLY.Rym.ZZS._he.ojN.p6V.zI7.iyn.Hsu"))
-}
-
-func testBbc() {
-    XCTAssertTrue(rules.displayNone(domain: "*bbc.com", selector: ".fc-consent-root"))
-    XCTAssertTrue(rules.displayNone(domain: "*bbc.com", selector: "#cookiePrompt"))
-    XCTAssertTrue(rules.displayNone(domain: "*bbc.com", selector: ".ssrcss-u3tmht-ConsentBanner.exhqgzu6"))
-}
-
-func testReddit() {
-    XCTAssertTrue(rules.displayNone(domain: "*reddit.com", selector: "._3q-XSJ2vokDQrvdG6mR__k"))
-    XCTAssertTrue(rules.displayNone(domain: "*reddit.com", selector: ".EUCookieNotice"))
-    XCTAssertTrue(rules.displayNone(domain: "*reddit.com", selector: ".XPromoPopup"))
-}
-*/
