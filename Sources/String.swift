7c283c393f22de8010bba37d91d10c4730de800b
vaux <zero.griffin@gmail.com>
2021-05-08 17:28:13 +0200

    Update tests

--- Sources/String.swift
+++ Sources/String.swift
@@ -29,7 +29,7 @@ extension String {
     private var url: (url: Self, browse: Browse)? {
         URL(string: self)
             .flatMap {
-                $0.scheme != nil || $0.host != nil || $0.query != nil
+                $0.scheme != nil && ($0.host != nil || $0.query != nil)
                     ? (self, .navigate)
                     : nil
             }
--- Tests/BrowseTests.swift
+++ Tests/BrowseTests.swift
@@ -10,114 +10,46 @@ final class BrowseTests: XCTestCase {
     
     func testSearch() {
         XCTAssertEqual("https://www.ecosia.org/search?q=hello%20world", "hello world".browse(engine: .ecosia) { url, _ in url })
-//        if case let .search(url) = Engine.ecosia.browse("hello world") {
-//            XCTAssertEqual("https://www.ecosia.org/search?q=hello%20world", url)
-//        } else {
-//            XCTFail()
-//        }
-//
-//        if case let .search(url) = Engine.google.browse("hello world") {
-//            XCTAssertEqual("https://www.google.com/search?q=hello%20world", url)
-//        } else {
-//            XCTFail()
-//        }
+        XCTAssertEqual("https://www.google.com/search?q=hello%20world", "hello world".browse(engine: .google) { url, _ in url })
     }
-    /*
+    
     func testURL() {
-        if case let .navigate(url) = Engine.ecosia.browse("https://github.com") {
-            XCTAssertEqual("https://github.com", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://github.com", "https://github.com".browse(engine: .google) { url, _ in url })
     }
     
     func testPartialURL() {
-        if case let .navigate(url) = Engine.ecosia.browse("github.com") {
-            XCTAssertEqual("https://github.com", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://github.com", "github.com".browse(engine: .google) { url, _ in url })
     }
     
     func testDeeplinks() {
-        if case let .navigate(url) = Engine.ecosia.browse("itms-services://?action=purchaseIntent&bundleId=incognit&productIdentifier=incognit.plus") {
-            XCTAssertEqual("itms-services://?action=purchaseIntent&bundleId=incognit&productIdentifier=incognit.plus",
-                           url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("itms-services://?action=purchaseIntent&bundleId=incognit&productIdentifier=incognit.plus", "itms-services://?action=purchaseIntent&bundleId=incognit&productIdentifier=incognit.plus".browse(engine: .google) { url, _ in url })
     }
     
     func testAmpersand() {
-        if case let .search(url) = Engine.google.browse("hello&world") {
-            XCTAssertEqual("https://www.google.com/search?q=hello%26world", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://www.google.com/search?q=hello%26world", "hello&world".browse(engine: .google) { url, _ in url })
     }
     
     func testPlus() {
-        if case let .search(url) = Engine.google.browse("hello+world") {
-            XCTAssertEqual("https://www.google.com/search?q=hello%2Bworld", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://www.google.com/search?q=hello%2Bworld", "hello+world".browse(engine: .google) { url, _ in url })
     }
     
     func testCaret() {
-        if case let .search(url) = Engine.google.browse("hello^world") {
-            XCTAssertEqual("https://www.google.com/search?q=hello%5Eworld", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://www.google.com/search?q=hello%5Eworld", "hello^world".browse(engine: .google) { url, _ in url })
     }
     
     func testSemiColon() {
-        if case let .search(url) = Engine.google.browse("hello:world") {
-            XCTAssertEqual("https://www.google.com/search?q=hello%3Aworld", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://www.google.com/search?q=hello%3Aworld", "hello:world".browse(engine: .google) { url, _ in url })
     }
     
     func testHttp() {
-        if case let .search(url) = Engine.google.browse("http") {
-            XCTAssertEqual("https://www.google.com/search?q=http", url)
-        } else {
-            XCTFail()
-        }
-        
-        if case let .search(url) = Engine.google.browse("https") {
-            XCTAssertEqual("https://www.google.com/search?q=https", url)
-        } else {
-            XCTFail()
-        }
-        
-        if case let .search(url) = Engine.google.browse("https:") {
-            XCTAssertEqual("https://www.google.com/search?q=https%3A", url)
-        } else {
-            XCTFail()
-        }
-        
-        if case let .search(url) = Engine.google.browse("https:/") {
-            XCTAssertEqual("https://www.google.com/search?q=https%3A/", url)
-        } else {
-            XCTFail()
-        }
-        
-        if case let .search(url) = Engine.google.browse("https://") {
-            XCTAssertEqual("https://www.google.com/search?q=https%3A//", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("https://www.google.com/search?q=http", "http".browse(engine: .google) { url, _ in url })
+        XCTAssertEqual("https://www.google.com/search?q=https", "https".browse(engine: .google) { url, _ in url })
+        XCTAssertEqual("https://www.google.com/search?q=https%3A", "https:".browse(engine: .google) { url, _ in url })
+        XCTAssertEqual("https://www.google.com/search?q=https%3A/", "https:/".browse(engine: .google) { url, _ in url })
+        XCTAssertEqual("https://www.google.com/search?q=https%3A//", "https://".browse(engine: .google) { url, _ in url })
     }
     
     func testDeeplink() {
-        if case let .navigate(url) = Engine.google.browse("macappstores://apps.apple.com/us/app/avocado-kanban/id1549855022?app=mac-app&extRefUrl2=https%3A%2F%2Favoca-do.github.io") {
-            XCTAssertEqual("macappstores://apps.apple.com/us/app/avocado-kanban/id1549855022?app=mac-app&extRefUrl2=https%3A%2F%2Favoca-do.github.io", url)
-        } else {
-            XCTFail()
-        }
+        XCTAssertEqual("macappstores://apps.apple.com/us/app/avocado-kanban/id1549855022?app=mac-app&extRefUrl2=https%3A%2F%2Favoca-do.github.io", "macappstores://apps.apple.com/us/app/avocado-kanban/id1549855022?app=mac-app&extRefUrl2=https%3A%2F%2Favoca-do.github.io".browse(engine: .google) { url, _ in url })
     }
- */
 }
