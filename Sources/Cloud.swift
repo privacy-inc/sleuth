238fd62751650c9be6c511ea3308ba7037954277
vaux <zero.griffin@gmail.com>
2021-05-28 14:45:09 +0200

    Remove static references

--- Sources/Cloud.swift
+++ Sources/Cloud.swift
@@ -2,9 +2,11 @@ import Foundation
 import Archivable
 
 extension Cloud where A == Archive {
-    public static let shared = Self(manifest: .init(
-                                        file: "Privacy.archive",
-                                        container: "iCloud.privacy",
-                                        prefix: "privacy_",
-                                        title: "Privacy"))
+    public static var new: Self {
+        .init(manifest: .init(
+                file: "Privacy.archive",
+                container: "iCloud.privacy",
+                prefix: "privacy_",
+                title: "Privacy"))
+    }
 }
