67824b82cda307277f894b11c0746dbff17cf704
vaux <zero.griffin@gmail.com>
2021-07-06 14:11:03 +0200

    Move tld.dat to resources

--- Tests/main.swift
+++ Tests/main.swift
@@ -3,7 +3,7 @@ import Foundation
 private let directory = URL(fileURLWithPath: FileManager.default.currentDirectoryPath)
 private let def = directory.appendingPathComponent("Sources/Tld.swift")
 private let suffix = directory.appendingPathComponent("Sources/Tld.Suffix.swift")
-private let dat = directory.appendingPathComponent("Tests/Tld.public_suffix_list.dat")
+private let dat = directory.appendingPathComponent("Resources/Tld.public_suffix_list.dat")
 
 let result = TldParser
     .parse(content: String(decoding: try! Data(contentsOf: dat), as: UTF8.self))
