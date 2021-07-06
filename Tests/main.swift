130171aec9d3687c168777f4f2c96bd922f6ed6e
vaux <zero.griffin@gmail.com>
2021-07-06 13:30:23 +0200

    Update main script

--- Tests/main.swift
+++ Tests/main.swift
@@ -5,4 +5,15 @@ private let def = directory.appendingPathComponent("Sources/Tld.swift")
 private let suffix = directory.appendingPathComponent("Sources/Tld.Suffix.swift")
 private let dat = directory.appendingPathComponent("Tests/Tld.public_suffix_list.dat")
 
-print(TldParser.parse(content: String(decoding: try! Data(contentsOf: dat), as: UTF8.self)))
+let result = TldParser
+    .parse(content: String(decoding: try! Data(contentsOf: dat), as: UTF8.self))
+try! Data(result
+        .enum
+        .utf8)
+    .write(to: def, options: .atomic)
+try! Data(result
+        .suffix
+        .utf8)
+    .write(to: suffix, options: .atomic)
+
+print("Updated enum and suffix!")
