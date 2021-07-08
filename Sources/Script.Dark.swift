e4997c285267a5033f2099bb82912adaaadf9eb9
vaux <zero.griffin@gmail.com>
2021-07-08 12:37:36 +0200

    Remove pseudo elements background

--- Sources/Script.Dark.swift
+++ Sources/Script.Dark.swift
@@ -8,8 +8,6 @@ function _privacy_incognit_make_dark(element) {
     const shadow = getComputedStyle(element).getPropertyValue("box-shadow");
     const text_color = element.style.color;
     const gradient = getComputedStyle(element).getPropertyValue("background").includes("gradient");
-    const before = getComputedStyle(element, ":before").getPropertyValue("background").includes("gradient");
-    const after = getComputedStyle(element, ":after").getPropertyValue("background").includes("gradient");
 
     if (element.tagName != "A" && text_color != "") {
         element.style.setProperty("color", "#cecccf", "important");
@@ -19,7 +17,7 @@ function _privacy_incognit_make_dark(element) {
         element.style.setProperty("box-shadow", "none", "important");
     }
 
-    if (gradient || before || after) {
+    if (gradient) {
         element.style.setProperty("background", "none", "important");
         element.style.setProperty("background-color", "rgba(37, 34, 40)", "important");
     } else if (parts.length > 3) {
@@ -57,6 +55,9 @@ a, a *, a:link *, a:visited *, a:hover *, a:active * {\
     border-color: #454248 !important;\
     outline-color: #454248 !important;\
     box-shadow: none !important;\
+}\
+::before, ::after {\
+    background: none !important;\
 }\
 @-webkit-keyframes _privacy_incognit_node {\
     from {\
