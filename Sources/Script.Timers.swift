b5e539ca89817cbf28156a779e30048ba98a233b
vaux <zero.griffin@gmail.com>
2021-07-25 13:38:33 +0200

    Update timers

--- Sources/Script.Timers.swift
+++ Sources/Script.Timers.swift
@@ -2,17 +2,37 @@ import Foundation
 
 extension Script {
     static let timers = """
-var _privacy_incognit_timeout = setTimeout;
+Promise = function() { };
+Worker = function() { };
 
-setTimeout = function(x, y) {
-    return -1;
+function _privacy_incognit_timers(w) {
+    const timeout = w.setTimeout;
+    const interval = w.setInterval;
+    
+    w.setTimeout = function(x, y) {
+        return -1;
+    }
+    
+    w.setInterval = function(x) {
+        return -1;
+    }
+    
+    var id_timeout = timeout(function() { }, 0);
+    var id_interval = interval(function() { }, 0);
+    
+    while (id_timeout--) {
+        clearTimeout(id_timeout);
+    }
+    
+    while (id_interval--) {
+        clearTimeout(id_interval);
+    }
 }
 
-var id = _privacy_incognit_timeout(function() { }, 0);
-_privacy_incognit_timeout = null;
+_privacy_incognit_timers(window);
 
-while (id--) {
-    clearTimeout(id);
+for (var index = 0; index <= window.frames.length; index++) {
+    _privacy_incognit_timers(window.frames[index]);
 }
 
 """
