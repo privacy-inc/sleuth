a0c8cbec08821d3e4abb260e12e5d8ce41ca94cd
vaux <zero.griffin@gmail.com>
2021-08-22 11:28:22 +0200

    Update packages

--- Sources/Cloud.swift
+++ Sources/Cloud.swift
@@ -1,17 +1,10 @@
-import Foundation
 import Archivable
 
 extension Cloud where A == Archive {
     public static var new: Self {
         .init(manifest: .init(
-                file: file,
+                file: "Privacy",
                 container: "iCloud.privacy",
                 prefix: "privacy_"))
     }
 }
-
-#if DEBUG
-    private let file = "Privacy.debug.archive"
-#else
-    private let file = "Privacy.archive"
-#endif
